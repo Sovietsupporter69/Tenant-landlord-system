7306c4bed2e2b79f9d07b3c33ec5d2f0561c7848
Jake <84923604+SecretiveShell@users.noreply.github.com>
2024-03-12 09:51:50 +0000

    added logged in index from template

---
+++ htdocs/logged-in/index.php
@@ -0,0 +1,16 @@
+<?php
+// these variables define properties about the page
+// and are managed automatically by the header
+// delete them if you do not need them in your file
+define("title", "logged in");
+// define("special_css", "page specific css");
+// define("special_script", "page specific script");
+
+require_once($_SERVER["DOCUMENT_ROOT"]."/private/document_head.php");
+?>
+
+<!-- set the contents of the body here -->
+
+<?php
+require_once($_SERVER["DOCUMENT_ROOT"]."/private/document_tail.php");
+?>
