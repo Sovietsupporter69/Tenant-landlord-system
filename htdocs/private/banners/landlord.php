2ca57b42254544b32f0bfdfec541662fbb6be846
AdamBrown04 <adambrown2004@outlook.com>
2024-03-14 11:51:43 +0000

    started work on burger menu

---
+++ htdocs/private/banners/landlord.php
@@ -0,0 +1,17 @@
+<div class="banner">
+    <a href="/"><span class="material-symbols-outlined">home</span><p>Home</p></a>
+            <div class="burger">
+                <div class="line"></div>
+                <div class="line"></div>
+                <div class="line"></div>
+            </div>
+<div>
+    <nav class="mainNav">
+        <menu>
+            <li><a href="">Home</a></li>
+            <li><a href=""></a></li>
+            <li><a href=""></a></li>
+        </menu>
+    </nav>
+</div>
+</div>
