49d355d8460b3642e67db3784a2eba53a8567104
Dec-246 <c3029680@my.shu.ac.uk>
2024-03-14 11:49:38 +0000

    started login page

--- htdocs/API/properties/page.php
+++ htdocs/API/properties/page.php
@@ -1,5 +1,5 @@
 <?php
 
-// this sould return paginated data as json
+// this should return paginated data as json
 
 ?>
---
+++ htdocs/log-in.php
@@ -0,0 +1,26 @@
+<?php
+
+
+require_once($_SERVER["DOCUMENT_ROOT"]."/private/document_head.php");
+require_once($_SERVER["DOCUMENT_ROOT"]."/private/banners/pre_auth.php")
+?>
+
+<main>
+
+
+<section class="RegisterPageTitle">
+<h1> Register</h1>
+
+<h3> Register as a tenant or a landlord </h3>
+</section>
+
+
+
+<div class="UsernameInputBox">
+
+
+</div>
+
+
+
+</main>
