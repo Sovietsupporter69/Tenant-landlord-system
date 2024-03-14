144aa717cc80f611548e23d576b5d1e4b0631f51
AdamBrown04 <adambrown2004@outlook.com>
2024-03-14 17:49:16 +0000

    continued work on burger menu, index navbar is currently the landlord one to test the burger menu.
    Visually correct, but burger menu itself is not working. Investigating why

--- htdocs/private/banners/landlord.php
+++ htdocs/private/banners/landlord.php
@@ -1,17 +1,26 @@
 <div class="banner">
-    <a href="/"><span class="material-symbols-outlined">home</span><p>Home</p></a>
+    <header>
+        <div class="bannerContainer">
+            <div>
+                <a href="/"><span class="material-symbols-outlined">home</span>
+                    <p>Home</p>
+                </a>
+            </div>
+
             <div class="burger">
                 <div class="line"></div>
                 <div class="line"></div>
                 <div class="line"></div>
             </div>
-<div>
-    <nav class="mainNav">
-        <menu>
-            <li><a href="">Home</a></li>
-            <li><a href=""></a></li>
-            <li><a href=""></a></li>
-        </menu>
-    </nav>
-</div>
+        </div>
+        <div>
+            <nav class="mainNav">
+                <menu>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">example text</a></li>
+                    <li><a href="">more example text</a></li>
+                </menu>
+            </nav>
+        </div>
+    </header>
 </div>
