08796831d0884adca57c20506fc9848b79268ca0
TerminalMan <84923604+SecretiveShell@users.noreply.github.com>
2024-03-17 22:45:34 +0000

    updated styling for the register page

--- htdocs/auth/register.php
+++ htdocs/auth/register.php
@@ -10,38 +10,38 @@ require_once($_SERVER["DOCUMENT_ROOT"]."/private/document_head.php");
 require_once($_SERVER["DOCUMENT_ROOT"]."/private/banners/pre_auth.php");
 ?>
 
-<main>
+<div id="main">
 
-    <section class="RegisterPageTitle">
-        <h1> Register</h1>
-        <h3> Register as a tenant or a landlord </h3>
+    <section class="title">
+        <h1>Register</h1>
+        <h3>Register as a tenant or a landlord </h3>
     </section>
 
     <form action="/API/login.php" method="post">
         <ul>
             <li>
                 <label for="name">Username:</label>
-                <input type="text" class="form-control" id="name">
+                <input type="text" class="form-control" id="name" required>
             </li>
             
             <li>
                 <label for="usr">Email:</label>
-                <input type="text" class="form-control" id="email">
+                <input type="text" class="form-control" id="email" required>
             </li>
             
             <li>
                 <label for="pwd">Password:</label>
-                <input type="password" class="form-control" id="pwd">
+                <input type="password" class="form-control" id="pwd" required>
             </li>
             
             <li>
                 <label for="pwd-conf">Confirm password:</label>
-                <input type="password" class="form-control" id="pwd-conf">
+                <input type="password" class="form-control" id="pwd-conf" required>
             </li>
             
             <li>
                 <label for="role">Role:</label>
-                <select class="form-control" id="role">
+                <select class="form-control" id="role" required>
                     <option>Tenant</option>
                     <option>Landlord</option>
                 </select>
@@ -52,7 +52,7 @@ require_once($_SERVER["DOCUMENT_ROOT"]."/private/banners/pre_auth.php");
             </li>
         </ul>
     </form>
-</main>
+</div id="main">
 
 <?php
 require_once ($_SERVER["DOCUMENT_ROOT"] . "/private/banners/footer.php");
