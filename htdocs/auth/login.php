b0569d496d71bae4354c683f53e685e6b786a3f8
TerminalMan <84923604+SecretiveShell@users.noreply.github.com>
2024-03-17 22:52:30 +0000

    added login based on registration
    its rushed because we need to demonstrate tomorrow

--- htdocs/auth/login.php
+++ htdocs/auth/login.php
@@ -3,14 +3,37 @@
 // and are managed automatically by the header
 // delete them if you do not need them in your file
 define("title", "log in");
-// define("special_css", "page specific css");
+define("special_css", "auth.css");
 // define("special_script", "page specific script");
 
 require_once($_SERVER["DOCUMENT_ROOT"]."/private/document_head.php");
 require_once($_SERVER["DOCUMENT_ROOT"]."/private/banners/pre_auth.php")
 ?>
 
-<!-- set the contents of the body here -->
+<div id="main">
+
+    <section class="title">
+        <h1>Login</h1>
+    </section>
+
+    <form action="/API/login.php" method="post">
+        <ul>
+            <li>
+                <label for="usr">Email:</label>
+                <input type="text" class="form-control" id="email" required>
+            </li>
+            
+            <li>
+                <label for="pwd">Password:</label>
+                <input type="password" class="form-control" id="pwd" required>
+            </li>
+
+            <li>
+                <input type="submit">
+            </li>
+        </ul>
+    </form>
+</div id="main">
 
 <?php
 require_once($_SERVER["DOCUMENT_ROOT"]."/private/banners/footer.php");
