1928ce1574a6e8153161c3e49d4026d0f923e03c
ScottForster400 <scottforster400@gmail.com>
2024-03-19 11:32:36 +0000

    Added css for desktop and mobile for tenant index

--- htdocs/logged-in/tenant/index.php
+++ htdocs/logged-in/tenant/index.php
@@ -13,18 +13,19 @@ require_once($_SERVER["DOCUMENT_ROOT"]."/private/banners/tenant.php")
 <main>
     <section class="leased-properties">
         <h2>Leased Properties</h2>
-        <div class="property">
-            <div class="lease-image">
-                <img src="test-property" alt="property">
+        <a href="">
+            <div class="property">
+                <div class="lease-image">
+                    <img src="../../assets/test-property.webp" alt="property">
+                </div>
+                <div class="lease-info">
+                    <h3>Property Name</h3>
+                    <p>Property:63 boston street</p>
+                    <p>Postcode:SH2 5RD</p>
+                    <p>Next Rent:13/04/23</p>
+                </div>
             </div>
-            <div class="lease-info">
-                <p>Property:63 boston street</p>
-                <p>Postcode:SH2 5RD</p>
-                <p>Next Rent:13/04/23
-            </div>
-
-        </div>
-
+        </a>
     </section>
 </main>
 
