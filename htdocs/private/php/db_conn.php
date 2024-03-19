18cfc694bc82bab86127c34c75a72de0837179f9
TerminalMan <84923604+SecretiveShell@users.noreply.github.com>
2024-03-19 09:35:22 +0000

    added initial database implementation
    using the mysql workbench auto generated code.
    the details match the python script

--- htdocs/private/php/db_conn.php
+++ htdocs/private/php/db_conn.php
@@ -1,5 +1,16 @@
 <?php
 
-// define mysqli/ORM model here
+$host="127.0.0.1";
+$port=3306;
+$socket="";
+$user="tenant-landlord";
+$password="tenant-landlord";
+$dbname="tenant-landlord";
+
+$con = new mysqli($host, $user, $password, $dbname, $port, $socket)
+	or die ('Could not connect to the database server' . mysqli_connect_error());
+
+//$con->close();
+
 
 ?>
