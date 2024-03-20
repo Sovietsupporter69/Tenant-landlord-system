8f1c00b065af8ae6c9bbde429920cb96bf9d70e7
TerminalMan <84923604+SecretiveShell@users.noreply.github.com>
2024-03-20 11:28:42 +0000

    add a way to verify user is logged in

---
+++ htdocs/private/php/check_auth.php
@@ -0,0 +1,19 @@
+<?php 
+
+if(!isset($_COOKIE['auth'])) {
+    header("Location: /auth/login.php?expired");
+    die();
+}
+
+$secret = $_COOKIE['auth'];
+
+require_once($_SERVER["DOCUMENT_ROOT"]."/private/php/redis.php");
+
+$userid = $redis_client->get($secret);
+
+if (!isset($userid)) {
+    header("Location: /auth/login.php?expired");
+    die();
+}
+
+?>
