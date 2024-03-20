b4e4b7944f40793532e3753ff94caab6552f1574
TerminalMan <84923604+SecretiveShell@users.noreply.github.com>
2024-03-20 09:56:17 +0000

    rename database variables to avoid conflicts

--- htdocs/private/php/db_conn.php
+++ htdocs/private/php/db_conn.php
@@ -1,13 +1,13 @@
 <?php
 
-$host="127.0.0.1";
-$port=3306;
-$socket="";
-$user="tms";
-$password="tms";
-$dbname="tms";
+$mysqli_host="127.0.0.1";
+$mysqli_port=3306;
+$mysqli_socket="";
+$mysqli_user="tms";
+$mysqli_password="tms";
+$mysqli_dbname="tms";
 
-$conn = new mysqli($host, $user, $password, $dbname, $port, $socket)
+$conn = new mysqli($mysqli_host, $mysqli_user, $mysqli_password, $mysqli_dbname, $mysqli_port, $mysqli_socket)
 	or die ('Could not connect to the database server' . mysqli_connect_error());
 
 //$conn->close();
