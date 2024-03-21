25fde8d3384dca66e1565b1d73757baf16322d40
ScottForster400 <scottforster400@gmail.com>
2024-03-21 10:41:02 +0000

    serachbox -exapnder wokring

---
+++ htdocs/js/search-expand.js
@@ -0,0 +1,8 @@
+function searchExpand() {
+    var x = document.getElementById("hidden-search");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  };
---
+++ htdocs/property-search.php
@@ -0,0 +1,58 @@
+<?php
+// these variables define properties about the page
+// and are managed automatically by the header
+// delete them if you do not need them in your file
+define("title", "Home page");
+// define("special_css", "page specific css");
+define("special_script", "search-expand.js");
+
+require_once($_SERVER["DOCUMENT_ROOT"]."/private/document_head.php");
+//require_once($_SERVER["DOCUMENT_ROOT"]."/private/banners/landlord.php");
+require_once($_SERVER["DOCUMENT_ROOT"]."/private/banners/pre_auth.php")
+?>
+<main>
+    <section class="search-box-properties">
+    <h3>Search For properties</h3>
+        <form action="" class="home-search">
+            <div class="top-search-property">
+                <div class="left-search-box-property">
+                    <div class="input-top">
+                        <label for="location">location</label>
+                        <input type="text" name="location" id="location">
+                    </div>
+                </div>
+                <div class="right-search-box-property">
+                    <div class="input-top">
+                        <label for="range">Range</label>
+                        <select name="range" id="range">
+                        <option value="5">5 miles</option>
+                        <option value="10">10 miles</option>
+                        <option value="15">15 miles</option>
+                        <option value="20">20+ miles</option>
+                    </select>
+                    </div>
+                </div>
+                <div class="search-expand">
+                    <span class="material-symbols-outlined drop-down" onclick="searchExpand()">arrow_drop_down</span>
+                </div>
+            </div>
+                <div class="hidden-search" id="hidden-search">
+                    <div class="input-top">
+                        <label for="min-price">Min Price</label>
+                        <input type="number" name="min-price" id="min-price">
+                    </div>
+                    <div class="input-top">
+                        <label for="max-price">Max Price</label>
+                        <input type="number" name="max-price" id="max-price">
+                    </div>
+                </div>
+            </div>
+            <input type="submit" value="search">
+        </form>
+    </section>
+</main>
+
+<?php
+require_once($_SERVER["DOCUMENT_ROOT"]."/private/banners/footer.php");
+require_once($_SERVER["DOCUMENT_ROOT"]."/private/document_tail.php");
+?>
