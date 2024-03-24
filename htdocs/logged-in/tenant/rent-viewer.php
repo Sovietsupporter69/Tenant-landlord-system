7162c8d28a2943daac45688ba937bb86c100c8e4
ScottForster400 <scottforster400@gmail.com>
2024-03-24 16:54:29 +0000

    rent viewer- html + css complete

---
+++ htdocs/logged-in/tenant/rent-viewer.php
@@ -0,0 +1,92 @@
+<?php
+// these variables define properties about the page
+// and are managed automatically by the header
+// delete them if you do not need them in your file
+define("title", "logged in");
+// define("special_css", "page specific css");
+// define("special_script", "page specific script");
+
+require_once($_SERVER["DOCUMENT_ROOT"]."/private/document_head.php");
+require_once($_SERVER["DOCUMENT_ROOT"]."/private/banners/tenant.php")
+?>
+
+<main>
+    <section class="rent">
+        <h2>Rent due this month</h2>
+        <div class="rent-due">
+            <p>£800</p>
+        </div>
+        <button>Pay</button>
+        <div class="payment-history">
+            <div class="payments">
+                <a href="">
+                    <div class="payment">
+                        <p>£250</p>
+                        <p>12/14/24</p>
+                        <button>Details</button>
+                    </div>
+                    <div class="payment">
+                        <p>£250</p>
+                        <p>12/14/24</p>
+                        <button>Details</button>
+                    </div>
+                    <div class="payment">
+                        <p>£250</p>
+                        <p>12/14/24</p>
+                        <button>Details</button>
+                    </div>
+                    <div class="payment">
+                        <p>£250</p>
+                        <p>12/14/24</p>
+                        <button>Details</button>
+                    </div>
+                    <div class="payment">
+                        <p>£250</p>
+                        <p>12/14/24</p>
+                        <button>Details</button>
+                    </div>
+                    <div class="payment">
+                        <p>£250</p>
+                        <p>12/14/24</p>
+                        <button>Details</button>
+                    </div>
+                    <div class="payment">
+                        <p>£250</p>
+                        <p>12/14/24</p>
+                        <button>Details</button>
+                    </div>
+                    <div class="payment">
+                        <p>£250</p>
+                        <p>12/14/24</p>
+                        <button>Details</button>
+                    </div>
+                    <div class="payment">
+                        <p>£250</p>
+                        <p>12/14/24</p>
+                        <button>Details</button>
+                    </div>
+                    <div class="payment">
+                        <p>£250</p>
+                        <p>12/14/24</p>
+                        <button>Details</button>
+                    </div>
+                    <div class="payment">
+                        <p>£250</p>
+                        <p>12/14/24</p>
+                        <button>Details</button>
+                    </div>
+                    <div class="payment">
+                        <p>£250</p>
+                        <p>12/14/24</p>
+                        <button>Details</button>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </section>
+</main>
+
+<?php
+require_once($_SERVER["DOCUMENT_ROOT"]."/private/banners/footer.php");
+require_once($_SERVER["DOCUMENT_ROOT"]."/private/document_tail.php");
+?>
