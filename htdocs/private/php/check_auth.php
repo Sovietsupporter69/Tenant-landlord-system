87fbd4e928d52d75721211938e0faa85d7986353
TerminalMan <84923604+SecretiveShell@users.noreply.github.com>
2024-03-28 21:51:03 +0000

    reset logout timer on page load

--- htdocs/private/php/check_auth.php
+++ htdocs/private/php/check_auth.php
@@ -9,7 +9,7 @@ $secret = $_COOKIE['auth'];
 
 require_once($_SERVER["DOCUMENT_ROOT"]."/private/php/redis.php");
 
-$userid = $redis_client->get("tms_auth_token:$secret");
+$userid = $redis_client->getex("tms_auth_token:$secret", 'ex', 20*60);
 
 if (!isset($userid)) {
     header("Location: /auth/login.php?expired");
