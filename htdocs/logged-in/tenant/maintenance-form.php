13933fe5d2da08cb93e94f651877916b2de3892c
ScottForster400 <scottforster400@gmail.com>
2024-03-28 20:51:35 +0000

    maintenance request- html css complete

---
+++ htdocs/logged-in/tenant/maintenance-form.php
@@ -0,0 +1,37 @@
+<?php
+require_once($_SERVER["DOCUMENT_ROOT"]."/private/php/check_auth.php");
+
+// these variables define properties about the page
+// and are managed automatically by the header
+// delete them if you do not need them in your file
+define("title", "logged in");
+// define("special_css", "page specific css");
+// define("special_script", "page specific script");
+
+require_once($_SERVER["DOCUMENT_ROOT"]."/private/document_head.php");
+require_once($_SERVER["DOCUMENT_ROOT"]."/private/banners/tenant.php")
+?>
+
+<main>
+    <section class="maintenance">
+        <h2>Maintenance Request Form</h2>
+        <div class="maintenance-request">
+            <form action="">
+                <label for="date">Date:</label>
+                <input type="date" name="date" id="date" required>
+                <label for="title">Title:</label>
+                <input type="text" name="title" id="title" required>
+                <label for="details">Details:</label>
+                <textarea name="details" id="details" cols="10" rows="10" required></textarea>
+                <div class="centre">
+                    <button>Submit</button>
+                </div>
+            </form>
+        </div>
+    </section>
+</main>
+
+<?php
+require_once($_SERVER["DOCUMENT_ROOT"]."/private/banners/footer.php");
+require_once($_SERVER["DOCUMENT_ROOT"]."/private/document_tail.php");
+?>
