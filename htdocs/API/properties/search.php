d86cba5afad734cf33cacf57a95bf0e3149400f7
TerminalMan <84923604+SecretiveShell@users.noreply.github.com>
2024-04-04 15:23:50 +0100

    finish query API

--- htdocs/API/properties/search.php
+++ htdocs/API/properties/search.php
@@ -6,62 +6,30 @@
 
 
 // validate arguments
-if (!isset($_GET['query'])) {
-    die("you must provide a query string");
-}
-
-$query = $_GET['query'];
 
 
-if (!isset($_GET['min-price'])) {
-    die("please provide a minimum price");
-}
-
-$min_price = $_GET['min-price'];
-
-
-if (!isset($_GET['max-price'])) {
-    die("please provide a maximum price");
-}
-
-$max_price = $_GET['max-price'];
-
-
-if (!isset($_GET['min-bedrooms'])) {
-    die("please provide a minimum number of bedrooms");
-}
-
-$min_bedrooms = $_GET['min-bedrooms'];
-
-
-if (!isset($_GET['max-bedrooms'])) {
-    die("please provide a maximum number of bedrooms");
-}
-
-$min_bedrooms = $_GET['max-bedrooms'];
-
+$query = $_GET['query'] ?? "";
+$min_price = $_GET['min-price'] ?? 0;
+$max_price = $_GET['max-price'] ?? PHP_INT_MAX;
+$min_bedrooms = $_GET['min-bedrooms'] ?? 0;
+$max_bedrooms = $_GET['max-bedrooms'] ?? PHP_INT_MAX;
 
 // select from database based on some query logic
 
+$query = "%$query%";
 
 require_once($_SERVER["DOCUMENT_ROOT"]."/private/php/db_conn.php");
 
-$stmt = $conn->prepare("SELECT * FROM tms.property WHERE ? < rental_price and ? > rental_price;");
-$stmt->bind_param("ii", $min_price, $max_price);
+$stmt = $conn->prepare("SELECT * FROM tms.property WHERE ? < rental_price and ? > rental_price and ? < num_bedrooms and ? > num_bedrooms and description like ?;");
+$stmt->bind_param("iiiis", $min_price, $max_price, $min_bedrooms, $max_bedrooms, $query);
 $stmt->execute();
 $result = $stmt->get_result();
-var_dump($result);
 
+$arr = array();
+while($row = $result->fetch_assoc()) {
+    $arr[] = $row;
+}
 
-$stmt = $conn->prepare("SELECT * FROM tms.property WHERE ? < num_bedrooms and ? > num_bedrooms;");
-$stmt->bind_param("ii", $min_bedrooms, $max_bedrooms);
-$stmt->execute();
-$result = $stmt->get_result();
-var_dump($result);
-
-
-
-
-echo json_encode($result);
+echo json_encode($arr);
 
 ?>
