2e6f3f4d385f32d3faa4acd544235f827414c20e
ScottForster400 <scottforster400@gmail.com>
2024-04-09 16:04:07 +0100

    added reference for image preview code

--- htdocs/js/img-upload-preview.js
+++ htdocs/js/img-upload-preview.js
@@ -1,3 +1,5 @@
+
+// used https://stackoverflow.com/questions/4459379/preview-an-image-before-it-is-uploaded to get image preview code
 var loadFile = function(event){
     console.dir("bruh");
     var output =document.getElementById('output');
