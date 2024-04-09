a380c890b17f111b1b321c2b8dd70c6f1bf0a50e
ScottForster400 <scottforster400@gmail.com>
2024-04-09 16:03:18 +0100

    add image preview working

--- htdocs/logged-in/landlord/add_images.php
+++ htdocs/logged-in/landlord/add_images.php
@@ -1,4 +1,11 @@
 <?php
+// these variables define properties about the page
+// and are managed automatically by the header
+// delete them if you do not need them in your file
+define("title", "logged in");
+// define("special_css", "page specific css");
+define("special_script", "img-upload-preview.js");
+
 require_once ($_SERVER["DOCUMENT_ROOT"] . "/private/document_head.php");
 require_once ($_SERVER["DOCUMENT_ROOT"] . "/private/banners/landlord.php");
 ?>
@@ -9,10 +16,10 @@ require_once ($_SERVER["DOCUMENT_ROOT"] . "/private/banners/landlord.php");
                 Add image to existing property
             </h3>
             <div class="lease-image">
-                <img src="/assets/test-property.webp" alt="property" id="img-slider-img">
+                <img src="" alt="property" id="output">
             </div>
             <form action="upload.php" method="post" enctype="multipart/form-data" class="img-upload-frm"> Select image to upload:
-                <input type="file" name="fileToUpload" id="fileToUpload" accept="image/jpeg, image/png" required>
+                <input type="file" name="fileToUpload" id="fileToUpload" accept="image/jpeg, image/png" required onchange="loadFile(event)">
                 <input type="submit" value="Upload Image" name="submit">
             </form>
         </section>
