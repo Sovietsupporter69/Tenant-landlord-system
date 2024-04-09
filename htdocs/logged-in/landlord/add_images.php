3c0daefae45314ed3af66bad22eb04ac2a92959a
ScottForster400 <scottforster400@gmail.com>
2024-04-09 12:22:23 +0100

    add image form - css altered

--- htdocs/logged-in/landlord/add_images.php
+++ htdocs/logged-in/landlord/add_images.php
@@ -2,25 +2,21 @@
 require_once ($_SERVER["DOCUMENT_ROOT"] . "/private/document_head.php");
 require_once ($_SERVER["DOCUMENT_ROOT"] . "/private/banners/landlord.php");
 ?>
-
-<div id="main">
-    <section class="title">
-        <h1> Add image </h1>
-
-        <h3>
-            Add image to existing property
-        </h3>
-    </section>
-</div>
-
-
-
-<body>
-<form action="upload.php" method="post" enctype="multipart/form-data"> Select image to upload:
-<input type="file" name="fileToUpload" id="fileToUpload" accept="image/jpeg, image/png" required>
-<input type="submit" value="Upload Image" name="submit">
-</form>
-
+    <main>
+        <section class="add-image">
+            <h1> Add image </h1>
+            <h3>
+                Add image to existing property
+            </h3>
+            <div class="lease-image">
+                <img src="/assets/test-property.webp" alt="property" id="img-slider-img">
+            </div>
+            <form action="upload.php" method="post" enctype="multipart/form-data" class="img-upload-frm"> Select image to upload:
+                <input type="file" name="fileToUpload" id="fileToUpload" accept="image/jpeg, image/png" required>
+                <input type="submit" value="Upload Image" name="submit">
+            </form>
+        </section>
+    </main>
 
 
 <?php
