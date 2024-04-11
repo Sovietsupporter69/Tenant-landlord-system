90e11d208e665afb296069cc284ea86beaf64bbb
ScottForster400 <scottforster400@gmail.com>
2024-04-11 10:45:02 +0100

    removed test variable from img-uplaod.js

--- htdocs/js/img-upload-preview.js
+++ htdocs/js/img-upload-preview.js
@@ -1,7 +1,6 @@
 
 // used https://stackoverflow.com/questions/4459379/preview-an-image-before-it-is-uploaded to get image preview code
 var loadFile = function(event){
-    console.dir("bruh");
     var output =document.getElementById('output');
     output.src = URL.createObjectURL(event.target.files[0]);
     output.onload = function(){
