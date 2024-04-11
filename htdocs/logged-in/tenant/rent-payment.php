a1c1948e7d1e5efe064be188e540d3e577a3ba93
ScottForster400 <scottforster400@gmail.com>
2024-04-11 10:06:31 +0100

    rent-payment html-css complete - paypal gone

--- htdocs/logged-in/tenant/rent-payment.php
+++ htdocs/logged-in/tenant/rent-payment.php
@@ -1,5 +1,5 @@
 <?php
-require_once($_SERVER["DOCUMENT_ROOT"]."/private/php/check_auth.php");
+//require_once($_SERVER["DOCUMENT_ROOT"]."/private/php/check_auth.php");
 
 
 // these variables define properties about the page
@@ -16,14 +16,30 @@ require_once($_SERVER["DOCUMENT_ROOT"]."/private/banners/tenant.php")
 <main>
     <section class="maintenance">
         <h2>Pay Rent</h2>
-        <div class="payment-option">
-            <img src="/assets/Gpay.png" alt="">
+        <div class="pay-form">
+        <form action="">
+            <label for="cardNum">Card Number:</label>
+            <input type="text" name="cardNum" id="cardNum"required minlength="16" maxlength="16">
+            <label for="cardName">Card Holder name:</label>
+            <input type="text" name="cardName" id="cradName"required>
+            <label for="amount">Amount(£):</label>
+            <input type="number" name="amount" id="amount" required min='5' max='1000000'>
+            <label for="cvc">CVC:</label>
+            <input type="text" name="cvc" id="cvc" required minlength="3" maxlength="3">
+            <label for="expiry">Expiry Date:</label>
+            <input type="month" name="expiry" id="expiry"required>
+            <div class="centre">
+                <button type="submit">submit</button>
+            </div>
+        </form>
         </div>
-        <div class="payment-option">
-            <img src="/assets/apple-pay.jpg" alt="">
-        </div>
-        <div class="payment-option">
-            <img src="/assets/pay-pal-2.jpg" alt="">
+        <div class="alt-pay">
+            <div class="payment-option">
+                <img src="/assets/Gpay.png" alt="">
+            </div>
+            <div class="payment-option">
+                <img src="/assets/apple-pay.jpg" alt="">
+            </div>
         </div>
     </section>
 </main>
