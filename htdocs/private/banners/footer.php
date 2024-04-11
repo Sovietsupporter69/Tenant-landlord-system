e2d5e15248335766d167ce9729d6888d5a0dcba5
TerminalMan <84923604+SecretiveShell@users.noreply.github.com>
2024-04-11 15:20:41 +0000

    add lsepi stuff to footer

--- htdocs/private/banners/footer.php
+++ htdocs/private/banners/footer.php
@@ -1,5 +1,7 @@
 <div class="footerContainer">
     <footer>
         <div>&copy; <?php echo date("Y"); ?> TMS ltd.</div>
+        <div><a href="mailto:leagal@tms.com">legal@tms.com</a></div>
+        <div><a href="/tos.php">Terms and conditions</a></div>
     </footer>
 </div>
