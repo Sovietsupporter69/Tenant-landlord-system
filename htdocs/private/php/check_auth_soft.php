ffb8c6b2ab4f4ea1f22bf340fde5fa65d7dcaba3
Jake <84923604+SecretiveShell@users.noreply.github.com>
2024-04-16 13:04:52 +0100

    improve auth

--- htdocs/private/php/check_auth_soft.php
+++ htdocs/private/php/check_auth_soft.php
@@ -1,20 +1,23 @@
 <?php
 
-$auth_secret = $_COOKIE['auth'];
-
-require_once($_SERVER["DOCUMENT_ROOT"]."/private/php/redis.php");
-
-$redis_userdata = $redis_client->getex("tms_user_session:$auth_secret", 'ex', 20*60);
-
 $user_logged_in = false;
-if (isset($redis_userdata)) {
-    $user_logged_in = true;
+
+if (isset($_COOKIE['auth'])) {
+    $auth_secret = $_COOKIE['auth'];
     
-    $redis_userdata = json_decode($redis_userdata, associative:true);
-    $userid = $redis_userdata['id'];
-    $username = $redis_userdata['username'];
-    $email = $redis_userdata['email'];
-    $user_type = $redis_userdata['type'];
+    require_once($_SERVER["DOCUMENT_ROOT"]."/private/php/redis.php");
+    
+    $redis_userdata = $redis_client->getex("tms_user_session:$auth_secret", 'ex', 20*60);
+    
+    if (isset($redis_userdata)) {
+        $user_logged_in = true;
+        
+        $redis_userdata = json_decode($redis_userdata, associative:true);
+        $userid = $redis_userdata['id'];
+        $username = $redis_userdata['username'];
+        $email = $redis_userdata['email'];
+        $user_type = $redis_userdata['type'];
+    }
 }
     
 ?>
