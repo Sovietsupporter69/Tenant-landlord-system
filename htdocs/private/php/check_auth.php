a071a81c959ddc6a6aa75769e06fc10ba177bc9d
Jake <84923604+SecretiveShell@users.noreply.github.com>
2024-04-16 11:38:36 +0100

    fix auth system to not infinite redirect when no cookies

--- htdocs/private/php/check_auth.php
+++ htdocs/private/php/check_auth.php
@@ -1,5 +1,10 @@
 <?php
 
+if(!isset($_COOKIE['auth'])) {
+    header("Location: /auth/login.php?expired");
+    die();
+}
+
 require_once($_SERVER["DOCUMENT_ROOT"]."/private/php/check_auth_soft.php");
 
 if (!$user_logged_in) {
