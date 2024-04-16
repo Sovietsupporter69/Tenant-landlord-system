2463a2cbf58d58de56b20474aaa057c4dc21b2d9
Dec-246 <c3029680@my.shu.ac.uk>
2024-04-16 12:18:17 +0100

    maintenance request details

--- htdocs/logged-in/tenant/maintenance-details.php
+++ htdocs/logged-in/tenant/maintenance-details.php
@@ -15,29 +15,29 @@ $stmt -> execute();
 $result = $stmt->get_result();
 
 require_once($_SERVER["DOCUMENT_ROOT"]."/private/document_head.php");
-require_once($_SERVER["DOCUMENT_ROOT"]."/private/banners/tenant.php")
+require_once($_SERVER["DOCUMENT_ROOT"]."/private/banners/tenant.php");
 
 function maintentance_details($title, $description, $urgency, $open_date, $close_date){
    
     $code = <<<EOT
     
-
     <div class="maintenance-summary">
     <p>$title</p>
     <p>$open_date</p>
-</div>
+    </div>
 
-<div class="maintenance-description">
+    <div class="maintenance-description">
     <h3>Details:</h3>
     <p>$description</p>
-</div>
+    </div>
 
-<div class="maintenance-status">
+    <div class="maintenance-status">
     <h3>Status:</h3>
     <p>- In Progress</p>
     <p>$urgency</p>
-</div>
-EOT;
+    </div>
+
+    EOT;
     echo($code);
 }
 ?>
