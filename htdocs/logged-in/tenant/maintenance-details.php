fd46de72c25ddc7a0f586b7b0c80b309d661b53e
Dec-246 <c3029680@my.shu.ac.uk>
2024-04-16 12:12:48 +0100

    creating maintenance request details

--- htdocs/logged-in/tenant/maintenance-details.php
+++ htdocs/logged-in/tenant/maintenance-details.php
@@ -1,5 +1,6 @@
 <?php
-//require_once($_SERVER["DOCUMENT_ROOT"]."/private/php/check_auth.php");
+require_once($_SERVER["DOCUMENT_ROOT"]."/private/php/check_auth.php");
+require_once($_SERVER["DOCUMENT_ROOT"]."/private/php/db_conn.php");
 
 // these variables define properties about the page
 // and are managed automatically by the header
@@ -8,25 +9,55 @@ define("title", "logged in");
 // define("special_css", "page specific css");
 // define("special_script", "page specific script");
 
+$stmt = $conn->prepare("SELECT maintenance_request.title, maintenance_request.description, maintenance_request.urgency, maintenance_request.open_date, maintenance_request.clsoe_date, maintenance_request.property_id FROM maintenance_request WHERE maintenance_request.tenant_id = ?");
+$stmt ->bind_param("s", $userid);
+$stmt -> execute();
+$result = $stmt->get_result();
+
 require_once($_SERVER["DOCUMENT_ROOT"]."/private/document_head.php");
 require_once($_SERVER["DOCUMENT_ROOT"]."/private/banners/tenant.php")
+
+function maintentance_details($title, $description, $urgency, $open_date, $close_date){
+   
+    $code = <<<EOT
+    
+
+    <div class="maintenance-summary">
+    <p>$title</p>
+    <p>$open_date</p>
+</div>
+
+<div class="maintenance-description">
+    <h3>Details:</h3>
+    <p>$description</p>
+</div>
+
+<div class="maintenance-status">
+    <h3>Status:</h3>
+    <p>- In Progress</p>
+    <p>$urgency</p>
+</div>
+EOT;
+    echo($code);
+}
 ?>
 
 <main>
     <section class="maintenance-details">
         <div class="maintenance-detail-contain">
-                <div class="maintenance-summary">
-                    <p>Broken Boiler</p>
-                    <p>14/05/23</p>
-                </div>
-                <div class="maintenance-description">
-                    <h3>Details:</h3>
-                    <p>Boiler stopped working please help me</p>
-                </div>
-                <div class="maintenance-status">
-                    <h3>Status:</h3>
-                    <p>- In Progress</p>
-                </div>
+
+        <?php
+                        if (mysqli_num_rows($result) > 0) {
+                            while($row = mysqli_fetch_assoc($result)) {
+                                render_maintenance($row['title'], $row['description'], $row['urgency'], $row['open_date'], $row['close_date']);
+                            }
+                        }
+                        else {
+                            echo("<p>You have no properties to display</p>");
+                        }
+                        ?>
+
+
             
         </div>
     </section>
