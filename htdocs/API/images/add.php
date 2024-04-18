011af282bba2740488aecccfd5d869cc019d0082
Jake <84923604+SecretiveShell@users.noreply.github.com>
2024-04-18 11:05:33 +0100

    do validation stuff on image delete

--- htdocs/API/images/add.php
+++ htdocs/API/images/add.php
@@ -2,4 +2,11 @@
 
 // add an image to a property
 
+require_once($_SERVER["DOCUMENT_ROOT"]."/private/php/check_auth.php");
+require_once($_SERVER["DOCUMENT_ROOT"]."/private/php/db_conn.php");
+
+if (!isset($_POST['property_id'])) {
+    die("property_id is required");
+}
+
 ?>
