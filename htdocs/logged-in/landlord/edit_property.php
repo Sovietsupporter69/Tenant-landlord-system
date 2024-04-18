1e19f8cb533db8417b92c8ae40052fb57a52ea18
Dec-246 <149159087+Dec-246@users.noreply.github.com>
2024-04-18 10:20:55 +0000

    edit property page

--- htdocs/logged-in/landlord/edit_property.php
+++ htdocs/logged-in/landlord/edit_property.php
@@ -12,6 +12,60 @@ require_once($_SERVER["DOCUMENT_ROOT"]."/private/document_head.php");
 
 <!-- set the contents of the body here -->
 
+<div id="main">
+
+    <section class="title">
+        <h1>Edit property</h1>
+        <h3>Edit a property of yours</h3>
+    </section>
+
+    <form action="/API/properties/edit.php" method="post">
+        <ul>
+            <li>
+                <label for="address">Address:</label>
+                <input type="text" class="form-control" id="address" name="address" required>
+            </li>
+
+            <li>
+                <label for="postcode">Postcode:</label>
+                <input type="text" class="form-control" id="postcode" name="postcode" required>
+            </li>
+
+            <li>
+                <label for="rpm">Rent per month:</label>
+                <input type="text" class="form-control" id="rpm" name="rpm" required>
+            </li>
+
+            <li>
+                <label for="propertyType">Type of property:</label>
+                <input type="text" class="form-control" id="propType" name="propType" required>
+            </li>
+
+            <li>
+                <label for="numOfBedrooms">Number of bedrooms:</label>
+                <input type="number" name="form-control" id="numOfBedrooms" name="numOfBedrooms">
+            </li>
+
+            <li>
+                <label for="numOfBathrooms">Number of bathrooms:</label>
+                <input type="number" name="form-control" id="numOfBathrooms" name="numOfBathrooms">
+            </li>
+
+            <li>
+                <label for="description">Descrption of property:</label>
+                <input type="text" class="form-control" id="description" name="description" required>
+            </li>
+
+            <li>
+                <input type="submit">
+            </li>
+        </ul>
+    </form>
+</div id="main">
+
+
+
+
 <?php
 require_once($_SERVER["DOCUMENT_ROOT"]."/private/banners/footer.php");
 require_once($_SERVER["DOCUMENT_ROOT"]."/private/document_tail.php");
